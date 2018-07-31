7c2909a6620976ec61366eead12f5715397c62bf
Swashbuckler1 <40607921+Swashbuckler1@users.noreply.github.com>
2018-07-31 12:13:09 -0400

    Update ocr_local_image.py

--- ocr_local_image.py
+++ ocr_local_image.py
@@ -2,7 +2,7 @@ import Algorithmia
 
 input = {
     "input":
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/STOP_sign.jpg/220px-STOP_sign.jpg",
+    "",
     "output": "data://.algo/character_recognition/TextDetectionCTPN/temp/receipt.png"
 }
 client = Algorithmia.client('simQubrq3ubzM21MWS71oDvbR011')
