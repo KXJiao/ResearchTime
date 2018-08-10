5590dc5a7eb3ba0cf3252e234aa800e8f5a247a5
Swashbuckler1 <40607921+Swashbuckler1@users.noreply.github.com>
2018-08-10 12:58:03 -0400

    Add files via upload

---
+++ dicClassifier.py
@@ -0,0 +1,21 @@
+text = open("subInfo.txt").read()
+
+
+def findCount(sub):
+    count = 0
+    terms = open(sub).readlines()
+    terms = [t.strip().lower() for t in terms]
+    for t in terms:
+        if t in text:
+            count += 1
+    return count
+
+
+subArr = []
+subArr.append((findCount("biology_terms.txt"), "biology_terms.txt"))
+subArr.append((findCount("chemistry_terms.txt"), "chemistry_terms.txt"))
+subArr.append((findCount("History_terms.txt"), "History_terms.txt"))
+subArr.append((findCount("physics_terms.txt"), "physics_terms.txt"))
+subArr = sorted(subArr)[::-1]
+print(subArr)
+print(subArr[0][1])
