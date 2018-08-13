a2f9e1246cfe13033b5f214fe51c5af3342f4856
Swashbuckler1 <40607921+Swashbuckler1@users.noreply.github.com>
2018-08-13 15:02:46 -0400

    Update dicClassifier.py

--- dicClassifier.py
+++ dicClassifier.py
@@ -16,6 +16,7 @@ subArr.append((findCount("biology_terms.txt"), "biology_terms.txt"))
 subArr.append((findCount("chemistry_terms.txt"), "chemistry_terms.txt"))
 subArr.append((findCount("History_terms.txt"), "History_terms.txt"))
 subArr.append((findCount("physics_terms.txt"), "physics_terms.txt"))
+subArr.append((findCount("math_terms.txt"), "math_terms.txt"))
 subArr = sorted(subArr)[::-1]
 print(subArr)
 print(subArr[0][1])
